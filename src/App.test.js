45788d422b810485d7e6d1ccadd59f214d38a93a
yurashni <yuriy.shnitsar@gmail.com>
2020-02-19 13:12:09 +0200

    delete one test

--- src/App.test.js
+++ src/App.test.js
@@ -9,9 +9,4 @@ test('renders learn react link', () => {
 });
 
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
