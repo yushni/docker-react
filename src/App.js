6ac23e46c7950612e83c2a1eb031735fef40a49d
yurashni <yuriy.shnitsar@gmail.com>
2020-02-20 11:37:03 +0200

    Move development process to feature branch!

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            HELLO MY NAME IS gtrdfdsafds!
+            Move development process to feature branch!
         </p>
         <a
           className="App-link"
